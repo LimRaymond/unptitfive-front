3107574ab050faea6cf41bfddabcacbc3684be6a
raysoju <raymond.lim672@gmail.com>
2020-09-29 19:25:30 +0200

    Changement de buildpacks from heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello World Front !</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to the start of the best JS application in the world !</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
