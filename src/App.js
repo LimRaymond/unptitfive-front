ffc2e9e59c749d1115495c590bdf9f955beec903
raysoju <raymond.lim672@gmail.com>
2020-09-29 19:19:18 +0200

    Error de get flavico

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the start of the best JS application in the world !</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World Front !</h1>
     </div>
   );
 }
