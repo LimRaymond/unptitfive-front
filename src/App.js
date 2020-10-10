2b409415765deb096d734d5b2162fe6c25c3fb71
raysoju <raymond.lim672@gmail.com>
2020-10-10 09:41:46 +0200

    Premier push, eslint non gerer pour l'instant

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Home from "./components/landing/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the start of the best JS application in the world !</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
