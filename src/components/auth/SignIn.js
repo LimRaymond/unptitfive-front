48bfd9cd834b2de93457ea16899ccff5dc4575a0
raysoju <raymond.lim672@gmail.com>
2020-10-30 15:00:49 +0100

    Ajout des messages d'erreur et de la gestion d'erreur niveau auth

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
 
 import {
   loginAction,
 } from '../../store/actions/authActions';
 
-const SignIn = () => {
-  const { register, handleSubmit } = useForm();
+const SignIn = (props) => {
+  const { register, handleSubmit, errors } = useForm();
   const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
 
   const onSubmit = (data) => {
     const userData = {
@@ -22,19 +23,28 @@ const SignIn = () => {
     <div className="form-container sign-in-container">
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <h1 className="form-title">Connexion</h1>
+        { errors.username && <p className="error-message">{errors.username.message}</p> }
         <input
           type="text"
           name="username"
           placeholder="Nom de compte"
           autoComplete="off"
-          ref={register}
+          ref={register({
+            required: 'Le champ nom de compte est vide',
+            minLength: { value: 1, message: 'Le champ nom de compte ne doit pas être vide'},
+          })}
         />
+        { errors.password && <p className="error-message">{errors.password.message}</p> }
         <input
           type="password"
           name="password"
           placeholder="Mot de passe"
-          ref={register}
+          ref={register({
+            required: 'Le champ mot de passe est vide',
+            minLength: { value: 1, message: 'Le champ nom de compte ne doit pas être vide'},
+          })}
         />
+        { !auth.errorRegister ? (<p className="success-message">{auth.auth.message}</p>) : null}
         <input type="submit" value="Se connecter" className="form-button" />
       </form>
     </div>
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useForm } from 'react-hook-form';
-import { useDispatch } from 'react-redux';
 
 import { registerAction } from '../../store/actions/authActions';
 
-const SignUp = () => {
+const SignUp = (props) => {
+  const { handleClickSignInButton } = props;
   const { register, handleSubmit, errors } = useForm();
+  const auth = useSelector((state) => state.auth);
   const dispatch = useDispatch();
 
-  const onSubmit = (data) => {
+  useEffect(() => {
+    if (!auth.errorRegister) {
+      handleClickSignInButton();
+    }
+  }, [auth.errorRegister]);
+
+  const onSubmit = async (data) => {
     const userData = {
       username: data.username,
       password: data.password,
@@ -16,29 +24,38 @@ const SignUp = () => {
       email: data.email,
     };
     dispatch(registerAction(userData));
-    window.location.reload();
   };
 
   return (
     <div className="form-container sign-up-container">
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
         <h1 className="form-title">Créer un compte</h1>
-
+        { errors.username && <p className="error-message">{errors.username.message}</p> }
         <input
           type="text"
           name="username"
           placeholder="Nom de compte"
           autoComplete="off"
-          ref={register}
+          ref={register({
+            required: 'Un nom de compte est nécessaire',
+            minLength: { value: 1, message: 'Le nom de compte doit contenir 1 caractères minimum'},
+          })}
         />
+        { errors.email && <p className="error-message">{errors.email.message}</p> }
         <input
-          type="email"
+          type="text"
           name="email"
           placeholder="Adresse Email"
           autoComplete="off"
-          ref={register}
+          ref={register({
+            required: 'Une adresse email est nécessaire',
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: "L'adresse email est invalide",
+            }
+          })}
         />
-        { errors.password && <p>{errors.password.message}</p> }
+        { errors.password && <p className="error-message">{errors.password.message}</p> }
         <input
           type="password"
           name="password"
@@ -54,6 +71,7 @@ const SignUp = () => {
           placeholder="Retapez votre mot de passe"
           ref={register}
         />
+        { auth.errorRegister ? (<p className="error-message">{auth.messageErrorRegister}</p>) : null}
         <input type="submit" value="S'inscrire" className="form-button" />
       </form>
     </div>
